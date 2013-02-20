6805e8a408b9bddb79ee330e7a5458880771231b
CurlyMoo <development@xbian.org>
2013-02-20 23:13:22 +0100

    Updated to XBian 1.0 Alpha 5 deb sources

--- kernel3.2.27/headers/create_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Create tarball
-rm data.tar.gz
-tar cvf data.tar
-cat status | grep ^A | sed -e 's/A\t//g' | sed -e 's/\(.*\)\{1,\}/"\1"/' | xargs tar rvf data.tar
-gzip -9 data.tar
--- kernel3.2.27/headers/create_paths.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat status | grep ^A | sed -e 's/A\t//g' | sed -e 's/\(.*\)\{1,\}/"\1\"/' | sed 's,/*[^/]\+/*$,",' > paths
-awk '!x[$0]++' paths > paths.new
-rm paths;
-while read p; do
-	echo -e "\tif [ ! -d $p ]; then\n\t\tmkdir -p $p;\n\tfi\n" >> paths
-done < paths.new
-rm paths.new
